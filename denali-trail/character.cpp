b68cde110a81fb87d0ae575a560e6d0b2be8549c
imthegngrbrdmn <imthegngrbrdmn@users.noreply.github.com>
2020-03-27 13:40:11 -0400

    make getters for character

--- denali-trail/character.cpp
+++ denali-trail/character.cpp
@@ -7,3 +7,28 @@ Character::Character(std::string="The Shadow")
 Character::~Character()
 {
 }
+
+std::string Character::name()
+{
+	return name_;
+}
+
+int Character::thirst()
+{
+	return thirst_;
+}
+
+int Character::hunger()
+{
+	return hunger_;
+}
+
+int Character::strength()
+{
+	return strength_;
+}
+
+int Character::health()
+{
+	return health_;
+}
