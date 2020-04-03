2c6d91893f12afbe515525f5c847872e72134f6f
imthegngrbrdmn <imthegngrbrdmn@users.noreply.github.com>
2020-04-03 15:20:18 -0400

    write bitwise way to remove things from inventory

--- denali-trail/inventory.cpp
+++ denali-trail/inventory.cpp
@@ -1,6 +1,16 @@
 #include "character.h"
 #include <cmath>
 
+bool Character::removeInventory(int item)
+{
+	if (checkInventory(item))
+	{
+		long long bItem = pow(2, item);
+		inventory_ = inventory_ ^ bItem;
+		return true;
+	}
+	return false;
+}
 bool Character::checkInventory(int item)
 {
 	long long bItem = pow(2, item);
