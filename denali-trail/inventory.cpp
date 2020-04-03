4ec5e861e56d64e8f9498f27a81a63c944db3740
imthegngrbrdmn <imthegngrbrdmn@users.noreply.github.com>
2020-04-03 15:20:39 -0400

    write bitwise way to add things to inventory

--- denali-trail/inventory.cpp
+++ denali-trail/inventory.cpp
@@ -1,6 +1,15 @@
 #include "character.h"
 #include <cmath>
-
+bool Character::addInventory(int item)
+{
+	if (!checkInventory(item))
+	{
+		long long bItem = pow(2, item);
+		inventory_ = inventory_ | bItem;
+		return true;
+	}
+	return false;
+}
 bool Character::removeInventory(int item)
 {
 	if (checkInventory(item))
