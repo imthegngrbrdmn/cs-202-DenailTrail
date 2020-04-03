ab17f0d59cfd67fe3a8b87b649e323595cfebbe9
imthegngrbrdmn <imthegngrbrdmn@users.noreply.github.com>
2020-04-03 15:30:11 -0400

    Write a key to understanding the inventory

--- denali-trail/inventory.cpp
+++ denali-trail/inventory.cpp
@@ -1,5 +1,20 @@
 #include "character.h"
 #include <cmath>
+/*
+	Inventory items should all be integers from 0-63 
+	representing bits of the 64 bit inventory. If a bit
+	is flipped to 1, that item is in the inventory, but
+	if it is flipped to 0, that item is not in the inventory.
+	
+	If you add an item that is in the inventory already, or you 
+	remove an item that isn't there, the function you called
+	returns false. 
+
+	Items are as follows:
+	0=
+	1=
+	...(REBECCA PUT THE ITEMS HERE)
+	*/
 bool Character::addInventory(int item)
 {
 	if (!checkInventory(item))
