2113e7a9275b28c29197249ef79e3b826f61ed2e
becca3 <32517979+becca3@users.noreply.github.com>
2020-04-05 00:29:10 +0100

    Filled vector with 10 items - option to add more if/when necessary

--- denali-trail/Items.cpp
+++ denali-trail/Items.cpp
@@ -3,3 +3,29 @@
 #include <vector>
 
 
+std::vector<std::string> items;
+
+std::string knife = "Knife";
+std::string candy = "Candy";
+std::string food = "Food";
+std::string water = "Water";
+std::string medicine = "Medicine";
+std::string bear_spray = "Bear Spray";
+std::string rocks = "Rocks";
+std::string notebook = "Notebook";
+std::string pen = "Pen";
+std::string empty_bottle = "Empty Bottle";
+
+void randomItem()
+{
+	items.push_back(knife);
+	items.push_back(candy);
+	items.push_back(food);
+	items.push_back(water);
+	items.push_back(medicine);
+	items.push_back(bear_spray);
+	items.push_back(rocks);
+	items.push_back(notebook);
+	items.push_back(pen);
+	items.push_back(empty_bottle);
+}
