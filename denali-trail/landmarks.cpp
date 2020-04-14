f07b85af7cf75c7dc4e18e389ea8259f8e4cd0bf
imthegngrbrdmn <imthegngrbrdmn@users.noreply.github.com>
2020-04-14 12:10:10 -0400

    Fix errors caused by git merge error

--- denali-trail/landmarks.cpp
+++ denali-trail/landmarks.cpp
@@ -1,10 +1,12 @@
 #include "page_design.h"
+#include <vector>
+#include <iostream>
 
 void landmark(Character& protag, int& distance)
 {
-	std::vector<std::pair<int,std::string>landmarks;
-	landmarks.push_back(std::make_pair(20,"Big Tree");
-	landmarks.push_back(std::make_pair(70,"Gigantic Rock");
+	std::vector<std::pair<int,std::string>> landmarks;
+	landmarks.push_back(std::make_pair(20,"Big Tree"));
+	landmarks.push_back(std::make_pair(70,"Gigantic Rock"));
 
 	for(auto landmark : landmarks)
 	{
