2e7e5c74fa1babcb15346b368c2bd467f0f700d5
becca3 <32517979+becca3@users.noreply.github.com>
2020-04-19 22:41:54 +0100

    Created images.h and images.cpp

---
+++ denali-trail/Images.h
@@ -0,0 +1,14 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+
+#include <iostream>
+
+void BearImg();
+void FoundItemImg();
+void LostImg();
+void IllnessImg();
+void WolfImg();
+void ShelterImg();
+void PersonImg();
+void PoisonImg();
